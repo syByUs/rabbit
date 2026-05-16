c32d3b1a69a356a079c7de44f2be4f1935d0820c
paupan <383509677@qq.com>
2026-05-16 09:32:12 +0800

    fix 修复编译问题 FAILURE: Build failed with an exception.
    * What went wrong:
    A problem occurred configuring project ':isar_flutter_libs'.
    > Could not create an instance of type com.android.build.api.variant.impl.LibraryVariantBuilderImpl.
       > Namespace not specified. Specify a namespace in the module's build file: /Users/pan/.pub-cache/hosted/pub.flutter-io.cn/isar_flutter_libs-3.1.0+1/android/build.gradle. See https://d.android.com/r/tools/upgrade-assistant/set-namespace for information about setting the namespace.
    
         If you've specified the package attribute in the source AndroidManifest.xml, you can use the AGP Upgrade Assistant to migrate to the namespace value in the build file. Refer to https://d.android.com/r/tools/upgrade-assistant/agp-upgrade-assistant for general information about using the AGP Upgrade Assistant.
    
    * Try:
    > Run with --stacktrace option to get the stack trace.
    > Run with --info or --debug option to get more log output.
    > Run with --scan to get full insights.
    > Get more help at https://help.gradle.org.
    
    BUILD FAILED in 23s
    Error: Gradle task assembleDebug failed with exit code 1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,39 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// --- 替换后的修复代码 ---
+subprojects {
+    // 监听所有子项目，当它们应用 Android 库插件或应用插件时执行
+    val fixNamespace: (Component) -> Unit = {
+        val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (android != null && android.namespace == null) {
+            if (project.name == "isar_flutter_libs") {
+                android.namespace = "dev.isar.isar_flutter_libs"
+            } else {
+                // 兜底：使用项目名作为 namespace
+                android.namespace = project.group.toString().ifEmpty { "com.example.${project.name.replace("-", "_")}" }
+            }
+        }
+    }
+
+    // 针对 Library 和 Application 插件都进行检查
+    plugins.withId("com.android.library") {
+        // 既然已经评估过了，我们直接尝试运行逻辑，或者包裹在针对 DSL 的配置中
+        val android = extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
+        if (android.namespace == null) {
+            android.namespace = if (project.name == "isar_flutter_libs") "dev.isar.isar_flutter_libs" else project.group.toString()
+        }
+    }
+    plugins.withId("com.android.application") {
+        val android = extensions.getByType(com.android.build.gradle.AppExtension::class.java)
+        if (android.namespace == null) {
+            android.namespace = project.group.toString()
+        }
+    }
+}
+// ----------------------
+
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
